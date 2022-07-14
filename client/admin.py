57e7ade86932b631880fb485c87092fc68592dd9
diyashrestha1999 <diyashrestha2000@gmail.com>
2022-07-14 23:19:39 +0545

    save button removed

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from .models import Client
 from django.contrib import admin
 
@@ -7,6 +8,15 @@ from django.contrib.auth.models import Group
 admin.site.unregister(get_user_model())
 admin.site.unregister(Group)
 
+class MyModelAdmin(admin.ModelAdmin):
+    ...
+    def change_view(self, request, object_id, extra_context=None):
+        if not request.user.is_superuser:
+            extra_context = extra_context or {}
+            extra_context['readonly'] = True
+
+        return super(MyModelAdmin, self).change_view(request, object_id, extra_context=extra_context)
+
 class PersonDataAdmin(admin.ModelAdmin):
     readonly_fields=('full_name', 'last_name', 'email', 'organisation','country','domain','phone_number','gender')
     search_fields = ['organisation']
@@ -15,4 +25,12 @@ class PersonDataAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
+    # def changeform_view(self,request,object_id=None,from_url='',extra_context=None):
+    #     extra_context=extra_context or {}
+    #     extra_context['show_save_and_continue']=False
+    #     extra_context['show_save']=False
+    #     return super(PersonDataAdmin,self).changeform_view(request,object_id,extra_context=extra_context)
+        
 admin.site.register(Client,PersonDataAdmin)
+
+
