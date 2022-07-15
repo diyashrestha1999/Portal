dc73da07f6c39065cd848e6e3ec59930aa5abcc6
diyashrestha1999 <diyashrestha2000@gmail.com>
2022-07-15 14:34:37 +0545

    almost completed

--- client/admin.py
+++ client/admin.py
@@ -19,32 +19,42 @@ class MyModelAdmin(admin.ModelAdmin):
         return super(MyModelAdmin, self).change_view(request, object_id, extra_context=extra_context)
 
 class PersonDataAdmin(admin.ModelAdmin):
-    readonly_fields=('full_name', 'last_name', 'email', 'organisation','organisation_size','date','country','domain','phone_number','gender','is_approved')
+    readonly_fields=('first_name', 'last_name', 'email', 'organisation','organisation_size','date','country','domain','phone_number','gender')
     search_fields = ['organisation']
-    list_display= ('full_name', 'email', 'organisation','date','domain','phone_number')
+    list_display= ('first_name', 'email', 'organisation','date','domain','phone_number','is_approved')
     def has_add_permission(self, request, obj=None):
         return False
 
     def changeform_view(self,request,object_id=None,from_url='',extra_context=None):
         extra_context=extra_context or {}
         extra_context['show_save_and_continue']=False
-        extra_context['show_save']=False
+        # extra_context['show_save']=False
         return super(PersonDataAdmin,self).changeform_view(request,object_id,extra_context=extra_context)
+    
+    change_form_template = "admin/change_form.html"
 
-# class ClientAdmin(admin.ModelAdmin):
-#     ...
-#     change_form_template = "entities/villain_changeform.html"
+    actions = ['approve_selected_clients', 'deny_selected_clients']
+    def approve_selected_clients(self,requst,queryset):
+        queryset.update(is_approved=True)
+
+    def deny_selected_clients(self,requst,queryset):
+        queryset.update(is_approved=False)
+
+    
+    approve_selected_clients.short_description = "Approve the selected clients"
+    deny_selected_clients.short_description = "Deny the selected clients"
 
 
-#     def response_change(self, request, obj):
-#         if "approve_client" in request.POST:
-         
-#             # self.message_user(request, "This villain is now unique")
-#             return HttpResponseRedirect("/admin/client/client/")
-#         return super().response_change(request, obj)
+    # def response_change(self, request, obj):
+    #     if "approve_client" in request.POST:
+    #         obj.is_approved = Truea
+    #         obj.save()
+    #         self.message_user(request, "This Client is now approved")
+    #         return HttpResponseRedirect(".")
+    #     return super().response_change(request, obj)
 
-   
+    
 admin.site.register(Client,PersonDataAdmin)
-# admin.site.register(ClientAdmin)
+
 
 
--- client/models.py
+++ client/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 
 class Client(models.Model):
-    CHOICE=[("Male","Male"),("Female","Female")]
+    CHOICE=[("Male","Male"),("Female","Female"),("Others","Others")]
     org=[("0-50 Employee","0-50 Employee"),("51-100 Employee","51-100 Employee"),
     ("101-150 Employee","101-150 Employee"),("151-200 Employee","151-200 Employee"),("200+ Employee","200+ Employee")]
-    full_name=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
     last_name= models.CharField(max_length=50)
     email=models.EmailField()
     organisation=models.CharField(max_length=50)
@@ -23,5 +23,5 @@ class Client(models.Model):
     organisation_size=models.CharField(max_length=50,choices=org)
 
     def __str__(self):
-        return f"Client: {self.full_name} {self.last_name} {self.date} {self.is_approved}"
+        return f"Client: {self.first_name} {self.last_name} {self.date} {self.is_approved}"
 
