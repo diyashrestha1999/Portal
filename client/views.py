df3a575c12c517a3d175e4efc9800c5f4093902f
diyashrestha1999 <diyashrestha2000@gmail.com>
2022-07-19 17:33:03 +0545

    testcase

--- client/views.py
+++ client/views.py
@@ -6,28 +6,28 @@ from .serializers import ClientCreateSerializer, ClientSerializer
 from django.core.exceptions import ValidationError
 
 
-def getclientinfo(request):
-    if request.method == 'POST':
-        first_name = request.POST["first_name"]
-        last_name = request.POST["last_name"]
-        email = request.POST["email"]
-        if Client.objects.filter(email=email).exists():
-            raise ValidationError("Email already exist")
-        organisation = request.POST["organisation"]
-        country = request.POST["country"]
-        phone_number = request.POST["phone_number"]
-        domain = request.POST["domain"]
-        gender = request.POST["gender"]
-        Client.objects.create(first_name=first_name,
-                              last_name=last_name,
-                              email=email,
-                              organisation=organisation,
-                              country=country,
-                              phone_number=phone_number,
-                              domain=domain,
-                              gender=gender
-                              )
-    return render(request, "html/loginform.html")
+# def getclientinfo(request):
+#     if request.method == 'POST':
+#         first_name = request.POST["first_name"]
+#         last_name = request.POST["last_name"]
+#         email = request.POST["email"]
+#         if Client.objects.filter(email=email).exists():
+#             raise ValidationError("Email already exist")
+#         organisation = request.POST["organisation"]
+#         country = request.POST["country"]
+#         phone_number = request.POST["phone_number"]
+#         domain = request.POST["domain"]
+#         gender = request.POST["gender"]
+#         Client.objects.create(first_name=first_name,
+#                               last_name=last_name,
+#                               email=email,
+#                               organisation=organisation,
+#                               country=country,
+#                               phone_number=phone_number,
+#                               domain=domain,
+#                               gender=gender
+#                               )
+#     return render(request, "html/loginform.html")
 
 class ClientCreateViewSet(CreateModelMixin, viewsets.GenericViewSet):
     queryset = Client.objects.all()
